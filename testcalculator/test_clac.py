d6989c183dc945c7ba8299168cfee32d342942f4
zaezjf <wqzpc@qq.com>
2020-07-11 16:41:17 +0800

    python pytest 测试实战（一）– 作业 第四次提交：将计算器实例放到setup_class

--- testcalculator/test_clac.py
+++ testcalculator/test_clac.py
@@ -7,35 +7,34 @@ c = yaml.safe_load(open("./data.yaml", encoding='utf-8'))
 
 class TestCalc:
 
+    def setup_class(self):
+        self.cal = Calculator()
+
     @pytest.mark.parametrize('read', c, ids=[f"{c[i][0]} + {c[i][1]}" for i in range(len(c))], indirect=True)
     @pytest.mark.add
     def test_add(self, read):
         print(f"计算：{read[0]} + {read[1]}，预期结果为：{read[2]}")
-        cal = Calculator()
-        temp_result = cal.add(read[0], read[1])
+        temp_result = self.cal.add(read[0], read[1])
         assert temp_result == read[2]
 
     @pytest.mark.parametrize('read', c, ids=[f"{c[i][0]} - {c[i][1]}" for i in range(len(c))], indirect=True)
     @pytest.mark.red
     def test_red(self, read):
         print(f"计算：{read[0]} - {read[1]}，预期结果为：{read[3]}")
-        cal = Calculator()
-        temp_result = cal.red(read[0], read[1])
+        temp_result = self.cal.red(read[0], read[1])
         assert temp_result == read[3]
 
     @pytest.mark.parametrize('read', c, ids=[f"{c[i][0]} * {c[i][1]}" for i in range(len(c))], indirect=True)
     @pytest.mark.mul
     def test_mul(self, read):
         print(f"计算：{read[0]} * {read[1]}，预期结果为：{read[4]}")
-        cal = Calculator()
-        temp_result = cal.mul(read[0], read[1])
+        temp_result = self.cal.mul(read[0], read[1])
         assert temp_result == read[4]
 
     @pytest.mark.parametrize('read', c, ids=[f"{c[i][0]} / {c[i][1]}" for i in range(len(c))], indirect=True)
     @pytest.mark.div
     def test_div(self, read):
         print(f"计算：{read[0]} ÷ {read[1]}，预期结果为：{read[5]}")
-        cal = Calculator()
-        temp_result = cal.div(read[0], read[1])
+        temp_result = self.cal.div(read[0], read[1])
         assert temp_result == read[5]
 
