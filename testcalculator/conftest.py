eb46ec922de2a5b756efb5f25d062a03c6cd5b75
zaezjf <wqzpc@qq.com>
2020-07-11 15:30:00 +0800

    python pytest 测试实战（一）– 作业 第三次提交：根据用例输入数据自动生成用例标题

--- testcalculator/conftest.py
+++ testcalculator/conftest.py
@@ -1,10 +1,8 @@
 import pytest
-import yaml
 
 
-@pytest.fixture(params=(yaml.safe_load(open("./data.yaml", encoding='utf-8'))), autouse=True)
+@pytest.fixture(autouse=True)
 def read(request):
     print("开始计算")
-    # print(type(request.param))
     yield request.param
     print("计算结束")
--- testcalculator/test_clac.py
+++ testcalculator/test_clac.py
@@ -1,9 +1,13 @@
 import pytest
+import yaml
 from testcalculator.calc import Calculator
+
+c = yaml.safe_load(open("./data.yaml", encoding='utf-8'))
 
 
 class TestCalc:
 
+    @pytest.mark.parametrize('read', c, ids=[f"{c[i][0]} + {c[i][1]}" for i in range(len(c))], indirect=True)
     @pytest.mark.add
     def test_add(self, read):
         print(f"计算：{read[0]} + {read[1]}，预期结果为：{read[2]}")
@@ -11,6 +15,7 @@ class TestCalc:
         temp_result = cal.add(read[0], read[1])
         assert temp_result == read[2]
 
+    @pytest.mark.parametrize('read', c, ids=[f"{c[i][0]} - {c[i][1]}" for i in range(len(c))], indirect=True)
     @pytest.mark.red
     def test_red(self, read):
         print(f"计算：{read[0]} - {read[1]}，预期结果为：{read[3]}")
@@ -18,6 +23,7 @@ class TestCalc:
         temp_result = cal.red(read[0], read[1])
         assert temp_result == read[3]
 
+    @pytest.mark.parametrize('read', c, ids=[f"{c[i][0]} * {c[i][1]}" for i in range(len(c))], indirect=True)
     @pytest.mark.mul
     def test_mul(self, read):
         print(f"计算：{read[0]} * {read[1]}，预期结果为：{read[4]}")
@@ -25,6 +31,7 @@ class TestCalc:
         temp_result = cal.mul(read[0], read[1])
         assert temp_result == read[4]
 
+    @pytest.mark.parametrize('read', c, ids=[f"{c[i][0]} / {c[i][1]}" for i in range(len(c))], indirect=True)
     @pytest.mark.div
     def test_div(self, read):
         print(f"计算：{read[0]} ÷ {read[1]}，预期结果为：{read[5]}")
